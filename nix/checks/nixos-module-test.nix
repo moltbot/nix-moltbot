jeanlucthumm/nix-openclaw

feat(nixos): add assertions for auth configuration
Fail early with clear messages when:
- Neither apiKeyFile nor oauthTokenFile is set for Anthropic
- gateway.auth.tokenFile is missing when mode is "token"
- gateway.auth.passwordFile is missing when mode is "password"

Update test to provide dummy tokens to satisfy assertions.

nix/checks/nixos-module-test.nix
@@ -21,9 +21,17 @@ pkgs.testers.nixosTest {
     services.clawdbot = {
       enable = true;
       package = pkgs.clawdbot-gateway;
-      # No API key - service will start but won't be fully functional
-      # That's fine for testing systemd/hardening
+      # Dummy token for testing - service won't be fully functional but will start
+      providers.anthropic.oauthTokenFile = "/run/clawdbot-test-token";
+      gateway.auth.tokenFile = "/run/clawdbot-gateway-token";
     };
+
+    # Create dummy token files for testing
+    system.activationScripts.clawdbotTestTokens = ''
+      echo "test-oauth-token" > /run/clawdbot-test-token
+      echo "test-gateway-token" > /run/clawdbot-gateway-token
+      chmod 600 /run/clawdbot-test-token /run/clawdbot-gateway-token
+    '';
 
     # Create a test file in /home to verify hardening
     users.users.testuser = {

moltbot/nix-moltbot

nix/checks/nixos-module-test.nix
@@ -21,9 +21,17 @@ pkgs.testers.nixosTest {
     services.openclaw = {
       enable = true;
       package = pkgs.openclaw-gateway;
-      # No API key - service will start but won't be fully functional
-      # That's fine for testing systemd/hardening
+      # Dummy token for testing - service won't be fully functional but will start
+      providers.anthropic.oauthTokenFile = "/run/openclaw-test-token";
+      gateway.auth.tokenFile = "/run/openclaw-gateway-token";
     };
+
+    # Create dummy token files for testing
+    system.activationScripts.openclawTestTokens = ''
+      echo "test-oauth-token" > /run/openclaw-test-token
+      echo "test-gateway-token" > /run/openclaw-gateway-token
+      chmod 600 /run/openclaw-test-token /run/openclaw-gateway-token
+    '';
 
     # Create a test file in /home to verify hardening
     users.users.testuser = {