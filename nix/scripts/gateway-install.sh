das-monki/nix-clawdbot

fix: bundle extensions dir

nix/scripts/gateway-install.sh
@@ -2,7 +2,11 @@
 set -e
 mkdir -p "$out/lib/clawdbot" "$out/bin"
 
+# Copy core files and bundled extensions (memory-core, etc.)
 cp -r dist node_modules package.json ui "$out/lib/clawdbot/"
+if [ -d "extensions" ]; then
+  cp -r extensions "$out/lib/clawdbot/"
+fi
 
 if [ -z "${STDENV_SETUP:-}" ]; then
   echo "STDENV_SETUP is not set" >&2
@@ -16,6 +20,14 @@ fi
 bash -e -c '. "$STDENV_SETUP"; patchShebangs "$out/lib/clawdbot/node_modules/.bin"'
 if [ -d "$out/lib/clawdbot/ui/node_modules/.bin" ]; then
   bash -e -c '. "$STDENV_SETUP"; patchShebangs "$out/lib/clawdbot/ui/node_modules/.bin"'
+fi
+# Patch shebangs in extensions node_modules if present
+if [ -d "$out/lib/clawdbot/extensions" ]; then
+  for ext_bin in "$out/lib/clawdbot/extensions"/*/node_modules/.bin; do
+    if [ -d "$ext_bin" ]; then
+      bash -e -c '. "$STDENV_SETUP"; patchShebangs "'"$ext_bin"'"'
+    fi
+  done
 fi
 
 # Work around missing dependency declaration in pi-coding-agent (strip-ansi).

moltbot/nix-moltbot

nix/scripts/gateway-install.sh
@@ -25,6 +25,14 @@ bash -e -c '. "$STDENV_SETUP"; patchShebangs "$out/lib/openclaw/node_modules/.bi
 if [ -d "$out/lib/openclaw/ui/node_modules/.bin" ]; then
   bash -e -c '. "$STDENV_SETUP"; patchShebangs "$out/lib/openclaw/ui/node_modules/.bin"'
 fi
+# Patch shebangs in extensions node_modules if present
+if [ -d "$out/lib/openclaw/extensions" ]; then
+  for ext_bin in "$out/lib/openclaw/extensions"/*/node_modules/.bin; do
+    if [ -d "$ext_bin" ]; then
+      bash -e -c '. "$STDENV_SETUP"; patchShebangs "'"$ext_bin"'"'
+    fi
+  done
+fi
 
 # Work around missing dependency declaration in pi-coding-agent (strip-ansi).
 # Ensure it is resolvable at runtime without changing upstream.